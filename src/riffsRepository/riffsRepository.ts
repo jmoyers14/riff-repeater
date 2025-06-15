6f27d1744dd3f3a8bee7151ecfe1132abe46436c
Jeremy Moyers <jeremy@jeremymoyers.com>
2025-06-14 18:22:29 -0700

    chrome storage tests passing

--- src/riffsRepository/riffsRepository.ts
+++ src/riffsRepository/riffsRepository.ts
@@ -1,6 +1,7 @@
-import { Riff, SavedRiff } from "../types";
+import { Riff, SavedRiff, SavedVideo, Video } from "../types";
 
 export interface RiffsRepositroy {
+    addVideo(video: Video): Promise<SavedVideo>;
     addRiff(videoId: string, riff: Riff): Promise<SavedRiff[]>;
     deleteRiff(videoId: string, riff: SavedRiff): Promise<SavedRiff[]>;
     getRiffs(videoId: string): Promise<SavedRiff[]>;
